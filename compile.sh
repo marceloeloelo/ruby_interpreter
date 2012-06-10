7237ceb3d1337b99a7b0ae781a3434e2a241de4d
Martin Rodriguez <martin@wyeworks.com>
2012-06-10 12:40:41 -0300

    Corrected clean and compile scripts.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
+echo "bison -vd ruby_grammar.y"
 bison -vd ruby_grammar.y
+echo "gcc -c ruby_grammar.tab.c"
+gcc -c ruby_grammar.tab.c
+echo "flex ruby_lex_analyzer.l"
 flex ruby_lex_analyzer.l
-gcc ruby_grammar.tab.c lex.yy.c -lfl -o interpreter
+echo "gcc -c lex.yy.c"
+gcc -c lex.yy.c
+echo "gcc -o interpreter lex.yy.o ruby_grammar.tab.o -lm"
+gcc -o interpreter lex.yy.o ruby_grammar.tab.o -lm
