815abaacc213352c31abfc8d007830db755e2e62
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-07-01 18:34:55 -0300

    reverting merge con cambios que cagaba todo

--- eval.c
+++ eval.c
@@ -116,8 +116,9 @@ struct ast* eval_ast(struct ast* node) {
       };
       case N_OP_EQUAL : {
                               struct identifier_node* left = (struct identifier_node*) node->left;
-                              put_sym(SYM_VAR, left->name, eval_ast(node->right), NULL);
-                              return NULL;
+                              struct ast* expression = NULL;
+                              put_sym(SYM_VAR, left->name, expression = eval_ast(node->right), NULL);
+                              return expression;
                               break;
       };
       case N_OP_PLUS_EQ : {
@@ -260,7 +261,7 @@ struct ast* eval_ast(struct ast* node) {
                                            (left->node_type == N_DOUBLE && right->node_type == N_INTEGER) ||
                                            (left->node_type == N_INTEGER && right->node_type == N_DOUBLE)) {
                                   if (double_value(right) != 0) {
-                                    s->ast = new_double_node(double_value(left) * double_value(right));
+                                    s->ast = new_double_node(double_value(left) / double_value(right));
                                   } else {
                                     /* ERROR !!! */
                                   }
@@ -340,7 +341,7 @@ struct ast* eval_ast(struct ast* node) {
                                 if (int_value(right) != 0) {
                                   return new_integer_node(int_value(left) / int_value(right));
                                 } else {
-                                  /* ERROR !!!! */
+                                  zero_division_error();
                                 }
 
                               // double / double || double / int || int / double
@@ -348,9 +349,9 @@ struct ast* eval_ast(struct ast* node) {
                                          (left->node_type == N_DOUBLE && right->node_type == N_INTEGER) ||
                                          (left->node_type == N_INTEGER && right->node_type == N_DOUBLE)) {
                                 if (double_value(right) != 0) {
-                                  return new_double_node(double_value(left) * double_value(right));
+                                  return new_double_node(double_value(left) / double_value(right));
                                 } else {
-                                  /* ERROR !!! */
+                                  zero_division_error();
                                 }
 
                               } else if (left->node_type != N_INTEGER &&
@@ -604,25 +605,33 @@ struct ast* eval_ast(struct ast* node) {
                               struct ast* left = eval_ast(node->left);
                               struct ast* right = eval_ast(node->right);
 
-                              // int, double
-                              if ((left->node_type == N_INTEGER  || left->node_type == N_DOUBLE) &&
-                                  (right->node_type == N_INTEGER || right->node_type == N_DOUBLE)) {
-                                if (double_value(left) == double_value(right)) {
-                                  return new_integer_node(0);
-                                } else if (double_value(left) > double_value(right)) {
-                                  return new_integer_node(1);
-                                } else {
-                                  return new_integer_node(-1);
+                              if (left != NULL && right !=NULL){
+                                // int, double
+                                if ((left->node_type == N_INTEGER  || left->node_type == N_DOUBLE) &&
+                                    (right->node_type == N_INTEGER || right->node_type == N_DOUBLE)) {
+                                  if (double_value(left) == double_value(right)) {
+                                    return new_integer_node(0);
+                                  } else if (double_value(left) > double_value(right)) {
+                                    return new_integer_node(1);
+                                  } else {
+                                    return new_integer_node(-1);
+                                  };
+                                // string <= string
+                                } else if (left->node_type == N_STRING_1 && right->node_type == N_STRING_1) {
+                                  int cmp = strcmp(string_value(left), string_value(right));
+                                  cmp = cmp > 0 ?  1 : cmp;
+                                  cmp = cmp < 0 ? -1 : cmp;
+                                  return new_integer_node(cmp);
+                                } else if (left->node_type == N_NIL || left->node_type == N_BOOL) {
+                                  no_method_error("<=>", left);
+                                } else if (right->node_type == N_NIL || right->node_type == N_BOOL){
+                                  no_method_error("<=>", right);
+                                } else{
+                                  return new_nil_node();
                                 };
-
-                              // string <= string
-                              } else if (left->node_type == N_STRING_1 && right->node_type == N_STRING_1) {
-                                return new_integer_node(strcmp(string_value(left), string_value(right)));
-
                               } else {
-                                no_method_error("<=>", left);
+                                no_method_error("<=>", NULL);
                               };
-
                               break;
       };
       case N_OP_CMP_NEG : {
