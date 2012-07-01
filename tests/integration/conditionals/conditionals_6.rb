c5e917aa9c422e5593f4b52eaf96157b065f58e7
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-07-01 12:12:01 -0300

    agregando tests conditionals, iterators

---
+++ tests/integration/conditionals/conditionals_6.rb
@@ -0,0 +1,21 @@
+if ( a = true )
+  puts "bien"
+  puts a
+end
+
+if ( "hola" )
+  puts "bien"
+end
+
+if ( a = "condition" )
+  puts a
+else
+  puts "mal"
+end
+
+if ( a = ( 1 == 1 ))
+  puts a
+else
+  puts "mal"
+end
+
