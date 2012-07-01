303e2610f6efa479566369df22b849acbb42ad24
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-06-30 23:46:16 -0300

    expressions_1 test

--- tests/integration/expressions/expressions_1.rb
+++ tests/integration/expressions/expressions_1.rb
@@ -1,8 +1,21 @@
-1 + 2 * 3 - 4.5
+var = 1 + 2 * 3 - 4.5
+puts var
 
-@var = 1
-@var = 1 + 2
-@var = 1 + 2 * 3
-@var = 1 + 2.3 / 4.5
-@var_1 = @var_var2 = "hello" + "goodbye"
-@var = "test"
+var = 1
+puts var
+
+var = 1 + 2
+puts var
+
+var = 1 + 2 * 3
+puts var
+
+var = 1 + 2.3 / 4.5
+puts var
+
+var_1 = var_var2 = "hello" + "goodbye"
+puts var_1
+puts var_var2
+
+var = "test"
+puts var
