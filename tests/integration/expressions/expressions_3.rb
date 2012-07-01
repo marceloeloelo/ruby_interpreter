46f881252e1e41415dcee6da07e23d8f8a30a508
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-06-30 23:58:21 -0300

    tests expressions_3

--- tests/integration/expressions/expressions_3.rb
+++ tests/integration/expressions/expressions_3.rb
@@ -1,5 +1,14 @@
 (1 + 1)
-@var = (1 + 1)
-@var_1 = (@var_2 = (@var_3 = 1 + 2))
+var = (1 + 1)
+puts var
+
+var_1 = (var_2 = (var_3 = 1 + 2))
+puts var_1
+puts var_2
+puts var_3
+
 (1 + 2) * (3 + 4)
+puts((1 + 2) * (3 + 4))
+
 ("test")
+puts(("test"))
