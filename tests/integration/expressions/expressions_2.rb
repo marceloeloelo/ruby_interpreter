7dcf759bd0dcb6da4260209285bc6da6bc9350fa
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-06-30 23:55:32 -0300

    expressions_2 tests

--- tests/integration/expressions/expressions_2.rb
+++ tests/integration/expressions/expressions_2.rb
@@ -2,16 +2,20 @@
 
 ;
 
-@var_1 = 1; @var_2 = @var_3 = 4; 1 + 1
+var_1 = 1; var_2 = var_3 = 4; 1 + 1
+puts var_1; puts var_2; puts var_3
 
-@var_4 = +1
+var_4 = +1
+puts var_4
 
-@var_5 = -1 + 2
+var_5 = -1 + 2
+puts var_5
 
-@var_6 = -1 + -1
+var_6 = -1 + -1
+puts var_6
 
-@var_7 = @var_8
+var_7 = var_6
+puts var_7
 
-@var_9 = nil
-
-@var_10 = self
+var_8 = nil
+puts var_8
